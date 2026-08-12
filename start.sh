16370ed0425d54e32e934f1c9ca4792ec0404d48
houegbelossiallode <houegbelossiallode@gmail.com>
2026-08-13 00:20:23 +0100

    modification de Dockerfile

--- start.sh
+++ start.sh
@@ -8,6 +8,10 @@ php artisan view:cache
 # Exécution des migrations de base de données
 # (Nécessaire si vous utilisez une base de données managée sur Render)
 php artisan migrate --force
+# Configuration dynamique du port d'Apache pour Render
+# (Render fournit la variable d'environnement $PORT, par défaut 10000)
+sed -i "s/Listen 80/Listen ${PORT:-80}/g" /etc/apache2/ports.conf
+sed -i "s/:80/:${PORT:-80}/g" /etc/apache2/sites-available/000-default.conf
 
 # Démarrage d'Apache en premier plan
 apache2-foreground
