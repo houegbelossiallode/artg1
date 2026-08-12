a4ce64900a51ab502fb6e6ffd6d4015db3d5f455
houegbelossiallode <houegbelossiallode@gmail.com>
2026-08-13 00:32:41 +0100

    modification de Dockerfile

--- start.sh
+++ start.sh
@@ -10,7 +10,7 @@ php artisan view:cache
 php artisan migrate --force
 # Configuration dynamique du port d'Apache pour Render
 # (Render fournit la variable d'environnement $PORT, par défaut 10000)
-sed -i "s/Listen 80/Listen ${PORT:-80}/g" /etc/apache2/ports.conf
+sed -i "s/Listen 80/Listen 0.0.0.0:${PORT:-80}/g" /etc/apache2/ports.conf
 sed -i "s/:80/:${PORT:-80}/g" /etc/apache2/sites-available/000-default.conf
 
 # Démarrage d'Apache en premier plan
